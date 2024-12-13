d4c69b941288853959094a1b94a2f034fdc66d69
Junyoung Kim <codesep8@gmail.com>
2024-12-13 20:06:58 +0900

    Update layout.js

--- public/layout.js
+++ public/layout.js
@@ -1,5 +1,20 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    $('html').attr('class', 'dark');
-} else {
-    $('html').attr('class', 'light');
+$(document).ready(function() {
+    const preferredTheme = localStorage.getItem('theme');
+
+    if (preferredTheme) {
+        $('html').attr('class', preferredTheme);
+    } else {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            $('html').attr('class', 'dark');
+        } else {
+            $('html').attr('class', 'light');
+        }
+    }
+});
+
+function toggleTheme() {
+    const currentTheme = $('html').attr('class');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    $('html').attr('class', newTheme);
+    localStorage.setItem('theme', newTheme); // 변경된 테마를 로컬 스토리지에 저장
 }

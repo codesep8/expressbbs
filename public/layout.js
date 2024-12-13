4cf4c2afe88b77c4bbdb76bb5edc70e3d4e269b1
Junyoung Kim <codesep8@gmail.com>
2024-12-13 20:32:23 +0900

    Update layout.js

--- public/layout.js
+++ public/layout.js
@@ -12,6 +12,24 @@ $(document).ready(function() {
     }
 });
 
+  $(document).ready(function () {
+    // Select 요소의 변경 이벤트를 감지
+    $('#display-config-theme\\.background').on('change', function () {
+      const selectedValue = $(this).val(); // 선택된 옵션 값
+      
+      // body에 클래스를 추가/제거하여 모드 전환
+      if (selectedValue === 'dark') {
+        $('html').removeClass('light').addClass('dark');
+        localStorage.setItem('theme', 'dark');
+      } else if (selectedValue === 'light') {
+        $('body').removeClass('dark').addClass('light');
+        localStorage.setItem('theme', 'light');
+      } else {
+        $('body').removeClass('dark light');
+      }
+    });
+});
+
 function toggleTheme() {
     const currentTheme = $('html').attr('class');
     const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
